bbff74722c084c1d479fd7f7414d727e6f7bae0c
Kelvin-T-Lu <61221557+Kelvin-T-Lu@users.noreply.github.com>
2023-02-11 14:05:32 -0500

    guidees

--- models/svm.py
+++ models/svm.py
@@ -43,7 +43,13 @@ class SVM:
 
         Hint: operate on mini-batches of data for SGD.
 
+        # https://www.baeldung.com/cs/svm-multiclass-classification (Concept)
+        # https://medium.com/@arsh1207/svm-implementation-from-scratch-in-python-8cf61a882ca8
         # https://ljvmiranda921.github.io/notebook/2017/02/11/multiclass-svm/
+        # https://cs231n.github.io/optimization-1/
+        # https://cs231n.github.io/linear-classify/
+        # https://towardsdatascience.com/svm-implementation-from-scratch-python-2db2fc52e5c2 
+        # https://github.com/qandeelabbassi/python-svm-sgd/blob/master/svm.py
 
         Parameters:
             X_train: a numpy array of shape (N, D) containing training data;
@@ -51,6 +57,30 @@ class SVM:
             y_train: a numpy array of shape (N,) containing training labels
         """
         # TODO: implement me
+        # ? Add bias function
+
+        # Weights - (Num_Classes, D)
+        # Rows - The weight vector for each class
+        # Column - The weight w.r.t. feature columns.
+        self.w = np.zeros((self.n_class, X_train.shape[1]))
+
+        X_train = np.asarray(X_train)  # Converting to Cupy's NDArray
+
+        for _ in range(self.epochs):
+            for index, data_row in enumerate(X_train):
+                # data_row = (1, D)
+                # weights = (Num_Classes, D)
+                y_pred = np.argmax(np.dot(data_row, self.w.T))
+    
+                # y_pred = 1
+                y_correct = y_train[index]
+                # print(f"Y_pred - {y_pred}, y_correct - {y_correct}")
+                if y_pred != y_correct:  # Wrong prediction
+                    # Update incorrect prediction w/ learnign rate.
+                    self.w[y_pred] -= self.lr * data_row
+                    # Update correct prediciton w/ learning rate.
+                    self.w[y_correct] += self.lr * data_row
+
         pass
 
     def predict(self, X_test: np.ndarray) -> np.ndarray:
